370081480cff4dbdeaf45716242621e72ca724fb
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-11-14 23:03:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,27 +2,22 @@ def using_include(array, element)
 array.include?(element)
 end
 
-def using_sort(alphabet)
-  alphabet = ["A", "C", "B"]
+def alphabet = ["A", "C", "B"]
 alphabet.sort
 end
 
-def using_reverse(alphabet)
-alphabet = ["A", "B", "C"]
+def alphabet = ["A", "B", "C"]
 alphabet.reverse
 end
 
-def using_first(alphabet)
-alphabet = ["A", "B", "C"]
+def alphabet = ["A", "B", "C"]
 alphabet.first
 end
 
-def using_last(alphabet)
-  alphabet = ["A", "B", "C"]
+def alphabet = ["A", "B", "C"]
   alphabet.last
 end
 
-def using_size(alphabet)
-  alphabet = ["A", "B", "C"]
+def alphabet = ["A", "B", "C"]
   alphabet.size
 end
